384b03eace75e40c38cdbfd6cd3811908fcd4a7d
luc <luc@forgotten.name>
2024-06-12 17:55:16 -0700

    lfg

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
-  description = "Let's Fucking Go";
+  description = "Lets Fucking Go";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, home-manager }: {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-
-    homeConfigurations = {
-      "ubuntu" = home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        homeDirectory = "/home/ubuntu";
-        username = "ubuntu";
-        configuration.imports = [ ./home-manger/lfg.nix ];
-      };
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.jdoe = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [ ./home-manager/lfg.nix ];
+      };
+    };
 }
