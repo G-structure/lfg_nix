01f558d6c4e89c3ce99fada5801f378a543bd9c4
luc <luc@forgotten.name>
2024-06-12 17:59:31 -0700

    default packages

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, defaultPackage.x86_64-linux, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
 
     in {
       homeConfigurations.jdoe = home-manager.lib.homeManagerConfiguration {
